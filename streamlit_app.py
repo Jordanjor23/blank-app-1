1b9295169632e929fcb0826dc2fad7d46a430454
Jordanjor23 <kelsey.woody23@gmail.com>
2025-03-10 20:25:08 +0000

    updated changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ page = st.sidebar.selectbox("Select a page", ['Home', "Data Overview", "EDA","Fi
 if page == "Home":
     st.title("📊 Starbucks Data Analysis Project")
     st.write("The Starbucks dataset provides nuturiontal information about the famous Starbucks menu. It contains 240 rows of Starbucks menu items ranging from (Frappuccino® Blended Coffee, Tazo® Tea Drinks, and  Classic Espresso Drinks). For each drink on the menu, the dataset includes nutritional information like calories, vitamin count, and cafeine level.")
-    st.image('/workspaces/blank-app-1/photo_1.jpg', caption="Starbucks Drinks")
+    st.image('photo_1.jpg', caption="Starbucks Drinks")
    
 
 elif page == "Data Overview":
@@ -40,7 +40,7 @@ if page == "Data Overview":
 
 
     tab1, tab2, tab3 = st.tabs(["Quick Overview of the Data","Describing the Data", "Shape of the Data"])
-    starbucks = pd.read_csv('/workspaces/blank-app-1/Copy_of_cleaned_starbucks.csv')
+    starbucks = pd.read_csv('Copy_of_cleaned_starbucks.csv')
     with tab1:
         st.title("Quick View of the Data ")
         head = pd.DataFrame(starbucks.head(30))
@@ -59,7 +59,7 @@ if page == "Data Overview":
 if page == "EDA":
     st.subheader("Select the type of visualization you'd like to explore:")
     eda_type = st.multiselect("Visualization Options", ['Histograms', 'Box Plots', 'Scatterplots', 'Bar Charts'])
-    starbucks = pd.read_csv ("/workspaces/blank-app-1/Copy_of_cleaned_starbucks.csv") 
+    starbucks = pd.read_csv ("Copy_of_cleaned_starbucks.csv") 
     obj_cols = starbucks.select_dtypes(include='object').columns.tolist()
     num_cols = starbucks.select_dtypes(include='number').columns.tolist()
     if 'Histograms' in eda_type:
@@ -82,6 +82,6 @@ if page == "Final Thoughts":
      
     st.subheader("Starbucks Coffee is Made for Everyone")
     st.write("This analysis provided insight into one of the most popular coffee shops in the wolrd, Starbucks. In the analysis we looked at key data points located in the '.csv' file which included beverage category, calories, and other relvant nutrional facts. Throughout the analysis, it became clear why Starbucks drinks are so popular. Starbuck's menu is versatile and has yummy, sweet, caffeinated, or uncaffeinated options, which makes it a good choice for everyone. In the EDA, I created visuals of how different beverages compared by calories, total fat, and caffeine levels. As a consistent Starbucks customer, I found these categories matter most the consumer because people want a drink that not only taste good, but is good for them. Overall, cool topic for an analysis. I hope you all enjoyed this. Feel free to DM me on Disco if you have any questions.")
-    st.image('/workspaces/blank-app-1/photo_2.jpg')
+    st.image('photo_2.jpg')
 
 
