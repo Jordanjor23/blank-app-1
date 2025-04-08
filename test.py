cc36fc3e472f5c92140ed777adf399fda5e3dce9
Jordanjor23 <kelsey.woody23@gmail.com>
2025-04-08 02:44:18 +0000

    test changes

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import plotly.subplots as sp
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from openpyxl import Workbook
+
 
 #read the csv file
 starbucks=pd.read_csv('/workspaces/blank-app-1/Copy_of_cleaned_starbucks.csv')
@@ -15,8 +17,8 @@ print(starbucks.head(30))
 starbucks_preview =pd.DataFrame(starbucks.head(30))
 print(starbucks_preview)
 
-#convert starbucks_preview to a excel file
-starbucks_preview.to_excel('/workspaces/blank-app-1/starbucks_preview.xlsx', index=False)
+
+
 
 
 
